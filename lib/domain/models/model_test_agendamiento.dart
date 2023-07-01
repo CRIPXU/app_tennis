bae83ba04de4c26a1593d6ef3cd523bd42d25ddd
Tu nombreCRIPXUPgit config --global user.email Tu <cristofer669galo@gmail.com>
2023-07-01 19:16:33 -0400

    implementacion de unit test 2

---
+++ lib/domain/models/model_test_agendamiento.dart
@@ -0,0 +1,13 @@
+class Agendamiento {
+  final int id;
+  final String cancha;
+  final String usuario;
+  final String fecha;
+
+  Agendamiento({
+    required this.id,
+    required this.cancha,
+    required this.usuario,
+    required this.fecha,
+  });
+}
